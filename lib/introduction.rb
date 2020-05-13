3b683975ce0b43341555c051b423d5b43ea87cdd
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-13 19:53:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,9 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
-
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
